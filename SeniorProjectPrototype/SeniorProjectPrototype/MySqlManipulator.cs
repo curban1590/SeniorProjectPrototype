16f87748aefe882aeaaff76418d5f1aee2e9c501
curban1590 <curban1590@users.noreply.github.com>
2019-02-26 19:57:47 -0500

    Added Project FIles

---
+++ SeniorProjectPrototype/SeniorProjectPrototype/MySqlManipulator.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using MySql.Data.MySqlClient;
+
+namespace SeniorProjectPrototype
+{
+    class MySqlManipulator
+    {
+            private MySqlConnection connection;
+            private string server;
+            private string database;
+            private string user;
+            private string password;
+            private string port;
+            private string connectionString;
+            private string sslM;
+
+            public void login()
+            {
+            
+                server = "sql9.freemysqlhosting.net";
+                database = "sql9280619";
+                user = "sql9280619";
+                password = "c1R8qVLfl1";
+                port = "3306";
+                sslM = "none";
+
+                connectionString = String.Format("server={0};port={1};user id={2}; password={3}; database={4}; SslMode={5}", server, port, user, password, database, sslM);
+
+                connection = new MySqlConnection(connectionString);
+                conexion();
+            }
+
+            private void conexion()
+            {
+                try
+                {
+                    connection.Open();
+
+                    MessageBox.Show("successful connection");
+
+                    connection.Close();
+                }
+                catch (MySqlException ex)
+                {
+                    MessageBox.Show(ex.Message + connectionString);
+                }
+            }
+
+        public bool loginCheck(string id, string zip)
+        {
+            bool isValid = false;
+
+            string command = "SELECT EmployeeID, EmployeeID FROM Employee WHERE EmployeeID = " + id; // + " AND EmployeeZip = " + zip + ";";
+
+            MySqlCommand myCommand = new MySqlCommand(command, connection);
+            connection.Open();
+
+            MySqlDataReader reader = myCommand.ExecuteReader();
+            
+            while (reader.Read())
+            {
+                if (id == reader.GetString("EmployeeID")) // && zip == reader.GetString("EmployeeZip"))
+                {
+                    isValid = true;
+                    break;
+                }
+            }
+
+            connection.Close();
+            return isValid;
+        }
+
+        }
+
+    }
+
---
+++ SeniorProjectPrototype/SeniorProjectPrototype/Page1.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SeniorProjectPrototype
+{
+    /// <summary>
+    /// Interaction logic for Page1.xaml
+    /// </summary>
+    public partial class Page1 : Page
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            string username = usernameTextBox.Text;
+            string password = passwordTextBox.Password;
+
+
+            MySqlManipulator mySqlManipulator = new MySqlManipulator();
+
+            mySqlManipulator.login();
+
+            bool isValid = mySqlManipulator.loginCheck(username, password);
+
+            Console.WriteLine(isValid);
+
+        }
+    }
+}
